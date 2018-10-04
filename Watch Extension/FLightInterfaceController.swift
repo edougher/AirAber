4e74cd9d1fb671f7d9584e7848d85b252be0af71
Erin Dougher <edougher@students.mchenry.edu>
2018-10-04 10:38:30 -0500

    Initial Commit

---
+++ Watch Extension/FLightInterfaceController.swift
@@ -0,0 +1,50 @@
+//
+//  FLightInterfaceController.swift
+//  Watch Extension
+//
+//  Created by Aaron Dougher on 10/3/18.
+//  Copyright © 2018 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class FLightInterfaceController: WKInterfaceController {
+  
+  @IBOutlet var flightLabel: WKInterfaceLabel!
+  @IBOutlet var routeLabel: WKInterfaceLabel!
+  @IBOutlet var boardingLabel: WKInterfaceLabel!
+  @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+  @IBOutlet var statusLabel: WKInterfaceLabel!
+  @IBOutlet var gateLabel: WKInterfaceLabel!
+  @IBOutlet var seatLabel: WKInterfaceLabel!
+
+  var flight: Flight? {
+    
+    didSet {
+      guard let flight = flight else { return }
+      
+      flightLabel.setText("Flight \(flight.shortNumber)")
+      routeLabel.setText(flight.route)
+      boardingLabel.setText("\(flight.number) Boards")
+      boardTimeLabel.setText(flight.boardsAt)
+      
+      if flight.onSchedule {
+        statusLabel.setText("On Time")
+      } else {
+        statusLabel.setText("Delayed")
+        statusLabel.setTextColor(.red)
+      }
+      gateLabel.setText("Gate \(flight.gate)")
+      seatLabel.setText("Seat \(flight.seat)")
+    }
+  }
+  override func awake(withContext context: Any?) {
+    super .awake(withContext: context)
+    
+    if let flight = context as? Flight {
+      self.flight = flight
+    }
+  }
+}
